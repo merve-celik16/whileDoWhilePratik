4b2627240e0ed3c74f3d21b1f8e4343d09fbbc46
merve-celik16 <mervedursunn34@gmail.com>
2024-10-09 02:07:40 +0300

    Proje dosyası ekle.

---
+++ whileDoWhilePratik/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// While Döngüsü
+
+// While döngüsünde koşul sağlandığı zaman çalışır.Eğer ilk adımda koşul sağlanmıyorsa döngü çalışmaz
+
+Console.Write("Lütfen limit için değer giriniz : ");
+int limit = Convert.ToInt32(Console.ReadLine());//kullanıcıdan limit değeri ister
+int sayac = 0;
+while (sayac <= limit) // koşulu döngünün başında kontrol eder.
+{
+    Console.WriteLine(" Ben bir Patika'lıyım.");
+    sayac++;
+}
+
+//Do-While Döngüsü
+
+//Do-while döngüsünden koşul sonda kontrol edildiği için yani koşul yanlış dahi olsa döngü en az bir kez çalışır.
+Console.Write("Lütfen limit için değer giriniz : ");
+int limit1 = Convert.ToInt32(Console.ReadLine());
+int sayac1 = 0;
+
+do 
+{
+    Console.WriteLine(" Ben bir Patika'lıyım.");
+    sayac++;
+
+}while (sayac1 <= limit1);//koşul kontrolü yapılır.
